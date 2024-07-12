5670c49d0c0cff31d8632420b94eb5692e82242c
ManiKrishnaa <130267764+ManiKrishnaa@users.noreply.github.com>
2024-07-12 17:47:14 +0530

    Update inventoryschema.js

--- models/inventoryschema.js
+++ models/inventoryschema.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/hope')
-
 const inventoryschema = new mongoose.Schema({
     itemname : {type : String,required : true},
     itemlevel : {type : String,required : true}
