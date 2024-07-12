c21a139b3f0f4c31f222a1bba52e08dcf613ef75
ManiKrishnaa <130267764+ManiKrishnaa@users.noreply.github.com>
2024-07-12 17:47:03 +0530

    Update donorschema.js

--- models/donorschema.js
+++ models/donorschema.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/hope')
-
 const donorschema = new mongoose.Schema({
     name : {type : String,required : true},
     username : {type : String,required : true,unique : true},
